7c0e72d9ff54a8088d4e8be840a9ccb0af3a1b8e
Juan David  Montoya <jdmontoaya049109@gmail.com>
2021-11-16 10:27:56 -0500

    Se adiciona reporte de cartera

---
+++ WebApp/Reportes/Facturas/Cartera.cs
@@ -0,0 +1,18 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace WebApp.Reportes.EntregaAdmisiones
+{
+    public partial class XtraReport1
+    {
+        public XtraReport1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+
+        }
+    }
+}
