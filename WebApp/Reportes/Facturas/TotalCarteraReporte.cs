c0673a8a2cad715e9c9e8813d8969c921837fb7f
Juan David  Montoya <jdmontoaya049109@gmail.com>
2021-11-18 09:29:36 -0500

    ADD

---
+++ WebApp/Reportes/Facturas/TotalCarteraReporte.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace WebApp.Reportes.Facturas
+{
+    public partial class TotalCarteraReporte
+    {
+        public TotalCarteraReporte()
+        {
+            InitializeComponent();
+        }
+    }
+}
