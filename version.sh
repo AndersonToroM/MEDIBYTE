550f96deb0756cc469d10c15ec12c4fdf2ce9224
edwin.aguiar <edwin.aguiar@siesa.com>
2022-01-05 22:50:05 -0500

    add

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
-cd /home/netcore/publish
+cd /home/net5/cloudonesoftapps
 chmod -R 777 ./siisoPublish
 cd ./siisoPublish
-cp appsettings.prod.json appsettings.json
+cp appsettings.dev.json appsettings.json
 cd ./Utils
 echo "{\"VersionApp\":\"$(date '+%Y%m%d')\",\"ParcheApp\":\"$(date '+%H%M')\"}" > infoApp.json
