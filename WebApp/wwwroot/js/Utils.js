1b9cc1a4b94d3065db16a14b4c005d607edc6d07
edwin.aguiar <edwin.aguiar@siesa.com>
2022-02-01 16:12:14 -0500

    cambios error enviar dian

--- WebApp/wwwroot/js/Utils.js
+++ WebApp/wwwroot/js/Utils.js
@@ -229,7 +229,6 @@ function PingServersIfErrorTimeOut() {
         "https://api.coindesk.com/v1/bpi/currentprice.json",
         "https://api.zippopotam.us/us/33162",
         "https://randomuser.me/api/",
-        "https://goweather.herokuapp.com/weather/colombia",
         "https://www.datos.gov.co/resource/xdk5-pm3f.json/?municipio=Cali"
     ];
     servers.forEach(server => {
