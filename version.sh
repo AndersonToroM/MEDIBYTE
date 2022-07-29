3cd15109ac2f05b7c9e7896f1ab00ce8cdf77454
edwinaguiar <edwinaguiar@gmail.com>
2022-07-28 19:31:38 -0500

    cambio versionador

--- version.sh
+++ version.sh
@@ -1,6 +1,4 @@
-cd /home/net5/cloudonesoftapps
-chmod -R 777 ./siisoPublish
-cd ./siisoPublish
-cp appsettings.dev.json appsettings.json
+cd ./publish
+cp appsettings.prod.json appsettings.json
 cd ./Utils
 echo "{\"VersionApp\":\"$(date '+%Y%m%d')\",\"ParcheApp\":\"$(date '+%H%M')\"}" > infoApp.json
