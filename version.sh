72930bcd59bf6e93297982b15be80bcc5fceb758
Edwin Antonio Aguiar Gomez <edwin.aguiar@outlook.com>
2023-02-16 20:42:43 -0500

    cambio de version sh para produccion

--- version.sh
+++ version.sh
@@ -1,4 +1,6 @@
-cd ./publish
+cd /home/cloudonesoftProd
+chmod -R 777 ./siisoapp
+cd ./siisoapp
 cp appsettings.prod.json appsettings.json
 cd ./Utils
 echo "{\"VersionApp\":\"$(date '+%Y%m%d')\",\"ParcheApp\":\"$(date '+%H%M')\"}" > infoApp.json
