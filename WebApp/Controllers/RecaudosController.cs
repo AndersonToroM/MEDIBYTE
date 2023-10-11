58780d201a2e12a6ad8424fd172bddb76c3d2343
Anderson Toro Muriel <gerencia@cloudonesoft.com>
2023-10-11 10:58:43 -0500

    Ajuste orden CarteraReporte.vsrepx (factura ascendente)

--- WebApp/Controllers/RecaudosController.cs
+++ WebApp/Controllers/RecaudosController.cs
@@ -16,6 +16,7 @@ using Blazor.BusinessLogic;
 using DevExpress.CodeParser.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Dominus.Backend.HttpClient;
 
 namespace Blazor.WebApp.Controllers
 {
