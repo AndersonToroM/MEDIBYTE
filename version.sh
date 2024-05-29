bd72809940b19f274daf019a21699357c4f88d1f
andersontoroM <anderson.toromuriel@gmail.com>
2024-05-29 11:01:45 -0500

    Ajustes archivo version.sh (desarrollo)

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 cd /home/cloudonesoftTest
 chmod -R 777 ./siisoapp
 cd ./siisoapp
-cp appsettings.prod.json appsettings.json
+cp appsettings.dev.json appsettings.json
 cd ./Utils
 echo "{\"VersionApp\":\"$(date '+%Y%m%d')\",\"ParcheApp\":\"$(date '+%H%M')\"}" > infoApp.json
